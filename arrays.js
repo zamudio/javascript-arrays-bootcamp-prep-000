e4f3abcc2e970e63e53212b28c656953c7fe87d3
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-10 02:02:29 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+}
