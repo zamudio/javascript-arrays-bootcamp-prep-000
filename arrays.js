37a2b03fe8e53bcac58705fb969e02ae185f1f5b
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-10 01:53:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array = array.unshift(element)
-  return
+  array.unshift(element)
+  return array
 }
