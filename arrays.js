f6833a4f344a0cd475433095d05efe6d8e765782
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-10 01:41:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray() {
+function addElementToBeginningOfArray(array, element) {
+  array = array.unshift(element)
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
   
 }
